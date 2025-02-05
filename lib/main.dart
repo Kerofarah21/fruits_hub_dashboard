897a0ca087f93e5faf8c3f735cc3b9ce9e254d40
Kerolos Farah <k.farah2124@gmail.com>
2025-02-05 10:32:02 +0200

    configure dashboard with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'core/helper_functions/on_generate_routes.dart';
 import 'core/utils/app_colors.dart';
 import 'features/dashboard/presentation/views/dashboard_view.dart';
+import 'firebase_options.dart';
 import 'generated/l10n.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
