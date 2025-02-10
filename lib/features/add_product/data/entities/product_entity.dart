3fe631ab76e37504fa9b6af465fa05d522c61625
Kerolos Farah <k.farah2124@gmail.com>
2025-02-10 10:31:54 +0200

    create product entity

---
+++ lib/features/add_product/data/entities/product_entity.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+class ProductEntity {
+  final String name;
+  final String description;
+  final String code;
+  final String price;
+  final String? imagePath;
+  final File image;
+  final bool isFeatured;
+
+  ProductEntity({
+    required this.name,
+    required this.description,
+    required this.code,
+    required this.price,
+    this.imagePath,
+    required this.image,
+    required this.isFeatured,
+  });
+}
